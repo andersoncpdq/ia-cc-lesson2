971ed7483903b09d1d9f2b4aeaa62c0be417ff15
Anderson Queiroz <anderson.queiroz@dongkuk.com.br>
2021-11-05 10:15:12 -0300

    docstring

--- src/generic_functions.py
+++ src/generic_functions.py
@@ -3,10 +3,25 @@ from collections import Counter
 
 
 def euclidean_distance(p1, p2):
+    """
+    Calcula a distancia euclidiana entre dois pontos.
+
+    :param p1: coordenada do ponto 1;
+    :param p2: coordenada do ponto 2;
+    :return: distancia euclidiana entre os pontos (p1 e p2).
+    """
     return np.linalg.norm(p1 - p2)
 
 
 def closest_coordinate(p1, lp):
+    """
+    Calcula qual a coordenada mais proxima de uma lista de
+    pontos a um ponto especifico.
+
+    :param p1: ponto especifico;
+    :param lp: lista de pontos;
+    :return: coordenada mais proxima na lista (lp) ao ponto (p1).
+    """
     pt = []
     min_dist = float('inf')
     for p in lp:
@@ -18,6 +33,13 @@ def closest_coordinate(p1, lp):
 
 
 def sort_string_list(str_lst, desc=False):
+    """
+    Ordena uma lista de strings considerando o tamanho de cada string.
+
+    :param str_lst: lista de strings;
+    :param desc: booleano para indicar ordem decrescente;
+    :return: a lista ordenada.
+    """
     if desc:
         str_lst = sorted(str_lst, key=len, reverse=True)
     else:
@@ -26,14 +48,38 @@ def sort_string_list(str_lst, desc=False):
 
 
 def count_string_occurrences(str_lst, str_key):
+    """
+    Contabiliza a quantidade de ocorrencias de uma string-alvo em uma
+    lista de strings.
+
+    :param str_lst: lista de strings;
+    :param str_key: string-alvo;
+    :return: numero de ocorrencias de str_key em str_lst.
+    """
     return str_lst.count(str_key)
 
 
 def occurrences_for_each_element(str_lst):
+    """
+    Contabiliza a quantidade de cada elemento na lista de strings.
+
+    :param str_lst: lista de strings;
+    :return: dictionary mapeando a contagem de cada elemento na lista.
+    """
     return Counter(str_lst)
 
 
 def proximity_analysis(str_lst, str_key, bias):
+    """
+    Realiza uma analise de proximidade de strings em uma lista de strings,
+    com base na string a ser analisada e o limiar de distancia de analise.
+
+    :param str_lst: lista de strings;
+    :param str_key: string de analise;
+    :param bias: limiar de distancia de analise;
+    :return: uma lista com as strings mais proximas da string de analise
+             (str_key) baseado no limiar de distancia (bias).
+    """
     str_idx = [i for i, s in enumerate(str_lst) if s == str_key]
     bias_idx = []
     for i in range(1, bias + 1):
@@ -47,10 +93,24 @@ def proximity_analysis(str_lst, str_key, bias):
 
 
 def no_repetition(lst):
+    """
+    Remove os elementos repetidos de uma lista de qualquer tipo de dados.
+
+    :param lst: lista de entrada;
+    :return: lista sem elementos repetidos.
+    """
     return list(dict.fromkeys(lst))
 
 
 def region_interest(mtx, c_roi, s_roi):
+    """
+    Extrai uma regiao de interesse (ROI) de uma matriz 2D.
+
+    :param mtx: matriz 2D de entrada;
+    :param c_roi: coordenada central da ROI (x, y);
+    :param s_roi: shape da ROI (h, w);
+    :return: recorte da matriz original (matriz ROI).
+    """
     x, y = c_roi
     h, w = s_roi
 
@@ -63,8 +123,25 @@ def region_interest(mtx, c_roi, s_roi):
 
 
 def roi_list(mtx, c_lst, s_lst):
+    """
+    Extrai multiplas ROIs de uma matriz 2D.
+
+    :param mtx: matriz 2D de entrada;
+    :param c_lst: lista dos centros das ROIs (x, y);
+    :param s_lst: lista de shapes das ROIs (h, w);
+    :return: lista de ROIs extraidas da matriz 2D de entrada.
+    """
     return [region_interest(mtx, c_lst[i], s_lst[i]) for i in range(len(c_lst))]
 
 
 def thresholding(mtx, bias):
+    """
+    Realiza a limiarizacao de uma matriz numerica. Para cada valor igual ou
+    superior ao limiar na matriz de entrada, é atribuído valor 1, caso contrario,
+    é atribuido valor 0, na mesma posição da matriz de retorno.
+
+    :param mtx: matriz de entrada;
+    :param bias: limiar;
+    :return: matriz binaria com as mesmas proporcoes da matriz de entrada.
+    """
     return np.vectorize(lambda x: 1 if x >= bias else 0)(mtx)
