b927b2ca14e3a93b89cbcbf675ac28195355f7a1
Luciano S. de Arruda <71555478+lucianoecp@users.noreply.github.com>
2021-11-20 11:50:17 -0300

    esqueleto do rpojeto
    a

---
+++ src/nlp_calc.py
@@ -0,0 +1,7 @@
+# 5. Implementar API 
+# 5.1 Calcular Term Frequency (TF):
+# 5.2 Calcular Document Frequency (Df):
+# 5.3 Calcular Inverse Document Frequency (IDF)
+# 5.4 Calcular TF-IDF
+#
+# 6. Gerar um arquivo csv.
