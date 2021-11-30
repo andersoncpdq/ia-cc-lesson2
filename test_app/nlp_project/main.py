ea0b8775b84f55b1f7202f4dde0a931401e8c478
gabriel-ino <chavesmartinsg@gmail.com>
2021-11-30 14:03:29 -0300

    lemantização e implementação para teste

---
+++ test_app/nlp_project/main.py
@@ -0,0 +1,17 @@
+from src.nlp_preproc import reading_pdfs, tokenize
+from pathlib import Path
+import argparse
+
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-path", "--path", default=str(Path(__file__).parent.resolve()) + '/pdf_files', help="Path to pdfs directory")
+
+    args = vars(ap.parse_args())
+    path = args['path']
+    list_filtered_texts = reading_pdfs(path)
+    tokenized_words = tokenize(list_filtered_texts)
+
+
+if __name__ == "__main__":
+    main()
