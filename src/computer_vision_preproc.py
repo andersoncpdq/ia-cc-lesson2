19979278d81d277a551d0cdc27d0bbca6c9bffea
gabriel-ino <chavesmartinsg@gmail.com>
2021-12-18 10:53:34 -0300

    Arquivo python CNN

--- src/computer_vision_preproc.py
+++ src/computer_vision_preproc.py
@@ -6,20 +6,16 @@ from keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
 import os
 
 
-def data_augmentation(PATH: str):
-    my_images = [cv2.imread(PATH + '/' + image) for image in sorted(os.listdir(PATH))]  # Getting images
+def data_augmentation(my_images):
+   # my_images = [cv2.imread(PATH + '/' + image) for image in sorted(os.listdir(PATH))]  # Getting images
     print(my_images)
     alpha = 1.02
     beta = 0.1
     adjusting_images = [cv2.convertScaleAbs(image, alpha=alpha, beta=beta) for image in my_images]  # Changing the contrast and the brightness of the images
     blur_images = [cv2.medianBlur(image, 1) for image in my_images]  # Images with blur
     rotate_image = [cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE) for image in my_images]  # Rotate images
-    for image in rotate_image:  # Testing new arrays
-        while True:
-            cv2.imshow(f'image', image)
-            key = cv2.waitKey(1)
-            if key == ord('q'):  # Press 'q' to pass to next image
-                break
+    array = [(my_images[pos], adjusting_images[pos], blur_images[pos], rotate_image[pos]) for pos in range(len(my_images))]
+    return array
 
 
 def normalize(PATH: str):
@@ -32,7 +28,6 @@ def normalize(PATH: str):
     return images
 
 
-data_augmentation('/home/bagriel/IAAcademy/iaacademy-cc/test_app/computer_vision/data_especular_crop/test_images/confluente')
 
 
 
